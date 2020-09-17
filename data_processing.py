7a97e575640b006a0a0f11591effa28c1011fde9
Longfei Li <lf97310@gmail.com>
2020-09-17 22:32:56 +0800

    feat: 增加对全部样本数据的处理

--- data_processing.py
+++ data_processing.py
@@ -43,7 +43,7 @@ def check_range(excel_data: DataFrame, range_dict: dict):
     # 遍历列，col_name 为列名，col_content 为该列的具体内容
     for col_name, col_content in excel_data.iteritems():
         # print("col_name: " + col_name[0])
-        if col_name[0] == '时间':
+        if col_name[0] in ['时间', '样本编号']:
             continue
 
         range_lower, range_upper = range_dict[col_name[0]]['lower_bound'], range_dict[col_name[0]]['upper_bound']
@@ -61,7 +61,7 @@ def check_range(excel_data: DataFrame, range_dict: dict):
 # (5) 拉伊达准则去除异常值
 def check_exceptional_data(excel_data: DataFrame):
     for col_name, col_content in excel_data.iteritems():
-        if col_name[0] == '时间':
+        if col_name[0] in ['时间', '样本编号']:
             continue
 
         # 求平均值
@@ -112,7 +112,7 @@ def remove_zero(excel_data: DataFrame):
 def fill_nan(excel_data: DataFrame):
     for col_name, col_content in excel_data.iteritems():
         # print("col_name: " + col_name[0])
-        if col_name[0] == '时间':
+        if col_name[0] in ['时间', '样本编号']:
             continue
 
         mean_val = col_content.mean()
@@ -124,11 +124,15 @@ def fill_nan(excel_data: DataFrame):
 # 读附件三，对 285 和 313 数据进行处理
 def process_original_data(path: str, sample_no: str, range_dict: dict):
     excel_data = None
+    data_count = 40
     if sample_no == '285':
         # nrows 不包含表头
         excel_data = pandas.read_excel(path, header=[1, 2], sheet_name=4, nrows=40)
     elif sample_no == '313':
         excel_data = pandas.read_excel(path, header=[1, 2], sheet_name=4, skiprows=range(3, 44), nrows=40)
+    elif sample_no == 'all':
+        excel_data = pandas.read_excel(path, header=[1, 2])
+        data_count = 325
 
     # 使用 nan 替换 0，方便 pandas 处理
     excel_data = excel_data.replace(0, numpy.nan, inplace=False)
@@ -138,18 +142,19 @@ def process_original_data(path: str, sample_no: str, range_dict: dict):
     # (5) 拉伊达准则筛选，异常值会设置为缺失值(nan)
     check_exceptional_data(excel_data)
     # (1) 去除缺失值(nan)数据较多的位点
-    excel_data = excel_data.dropna(axis=1, how='any', thresh=40 * 0.7, inplace=False)
+    excel_data = excel_data.dropna(axis=1, how='any', thresh=data_count * 0.7, inplace=False)
     # (3) todo: 使用（这一列的）平均值填充缺失值
     excel_data = fill_nan(excel_data)
 
-    # 添加平均值到文件末尾
-    avg_data = []
-    for col_name, col_content in excel_data.iteritems():
-        if col_name[0] == '时间':
-            avg_data.append("")
-        else:
-            avg_data.append(format(col_content.mean(), '.4f'))
-    excel_data.loc[40] = avg_data
+    if sample_no != 'all':
+        # 添加平均值到文件末尾
+        avg_data = []
+        for col_name, col_content in excel_data.iteritems():
+            if col_name[0] == '时间':
+                avg_data.append("")
+            else:
+                avg_data.append(format(col_content.mean(), '.4f'))
+        excel_data.loc[40] = avg_data
 
     return excel_data
 
@@ -157,7 +162,8 @@ def process_original_data(path: str, sample_no: str, range_dict: dict):
 if __name__ == '__main__':
     range_data = get_variable_range("/Users/faye/Downloads/数模题/附件四：354个操作变量信息.xlsx")
     # process_original_data("D:\\Downloads\\2020年中国研究生数学建模竞赛赛题\\2020年B题\\数模题\\附件三：285号和313号样本原始数据.xlsx")
-    processed_data = process_original_data("/Users/faye/Downloads/数模题/test.xlsx", "313", range_data)
+    # processed_data = process_original_data("/Users/faye/Downloads/数模题/test.xlsx", "313", range_data)
+    processed_data = process_original_data("/Users/faye/Downloads/数模题/test_325个样本数据的副本.xlsx", "all", range_data)
 
     # print(excel_data)
     pandas.DataFrame(processed_data).to_excel("/Users/faye/Desktop/output.xlsx", sheet_name='Sheet1', header=True)
