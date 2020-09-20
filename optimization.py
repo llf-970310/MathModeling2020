b0acb2615d06542832f622bdd00313d67e257b0b
Longfei Li <lf97310@gmail.com>
2020-09-20 15:11:44 +0800

    feat: 增加优化过程的记录，并导出到 excel

--- optimization.py
+++ optimization.py
@@ -1,5 +1,5 @@
+import copy
 import json
-from functools import cmp_to_key
 from pprint import pprint
 
 import joblib
@@ -24,8 +24,15 @@ def variable_list_sort(var_list, range_data, mapping_data):
 
 
 def greedy(sample_dict: dict, ron: float, row_index: int):
+    df = pandas.DataFrame()
+
     # 描述变化量
     delta_sum_dict = {}
+    # 优化流程（dict 列表）
+    tmp_dict = copy.deepcopy(sample_dict)
+    tmp_dict["ron损失"] = ron
+    tmp_dict["硫"] = check_sulfur(sample_dict, row_index, "3.2")
+    df = df.append(tmp_dict, ignore_index=True)
 
     initial_ron = ron
     range_data = get_variable_range("./data/354个操作变量信息.xlsx")
@@ -62,17 +69,23 @@ def greedy(sample_dict: dict, ron: float, row_index: int):
         new_ron = get_new_ron(new_sample, row_index)
 
         # print(low <= new_sample[variable_pick_no] <= high)
-        # print(check_sulfur(new_sample))
+        # print(check_sulfur(new_sample, row_index, "5"))
         # print(new_ron <= initial_ron)
+        # print()
 
         # 变更完满足如下条件：
         # 1. 变量仍在范围内
         # 2. 硫含量小于 5
         # 3. ron 损失有降低
-        if low <= new_sample[variable_pick_no] <= high and check_sulfur(new_sample) is True and new_ron <= ron:
+        if low <= new_sample[variable_pick_no] <= high and check_sulfur(new_sample, row_index, "5") is True and new_ron <= ron:
             ron = new_ron
             sample_dict = new_sample
             delta_sum_dict[variable_pick_no] += delta
+            # optiization_list.append(copy.deepcopy(sample_dict))
+            tmp_dict = copy.deepcopy(sample_dict)
+            tmp_dict["ron损失"] = ron
+            tmp_dict["硫"] = check_sulfur(sample_dict, row_index, "3.2")
+            df = df.append(tmp_dict, ignore_index=True)
         else:
             count += 1
             random += 1
@@ -86,21 +99,27 @@ def greedy(sample_dict: dict, ron: float, row_index: int):
         f.write(json.dumps(delta_sum_dict))
         f.write("\n\n")
 
+    df.to_excel("./data/result/" + str(row_index + 1) + ".xlsx", index=False, header=True)
     print("row_index: %d, original: %.5f, new: %.5f, percent: %.2f%%" % (row_index, initial_ron, ron, (initial_ron - ron) / initial_ron * 100))
     print(delta_sum_dict)
     print()
 
 
 # 判断硫含量
-def check_sulfur(sample_dict: dict):
-    model = joblib.load("./classification_model_s_5.pkl")
+def check_sulfur(sample_dict: dict, index: int, type: str):
     df_x = pandas.read_excel("./data/x2.xlsx")
-
     for key, val in sample_dict.items():
         if key in df_x.columns:
-            df_x.loc[0, key] = val
+            df_x.loc[index, key] = val
 
-    if model.predict(df_x[0:1])[0] == 1.0:
+    model = None
+    if type == "5":
+        model = joblib.load("./classification_model_s_5.pkl")
+    elif type == "3.2":
+        # model = joblib.load("./classification_model_s_3.2.pkl")
+        return get_rest_s_dict(df_x.to_dict('records')[index])
+
+    if model.predict(df_x[index:index+1])[0] == 1.0:
         return True
     else:
         return False
@@ -117,6 +136,26 @@ def get_new_ron(sample_dict: dict, index: int):
     return model.predict(df_x[index:index + 1])[0][0]
 
 
+def get_rest_s(x):
+    b0 = -17.1393441637
+    b = [-0.0575303098, 0.0043942943, -0.0000278449, -0.0328484906, 0.0176854694, -0.0022627343, 0.0239644130,
+         -0.0035410290, -0.2210221642, 0.0052330259, 0.0079804443, 0.0035524819, 0.0114921149, 1.1437657356,
+         -0.0331726202, 0.0148461304, 0.0110701114, 0.0144670189, -0.0099540070, 0.0330228034, 0.0005910554]
+
+    y = b0
+    for i in range(len(b)):
+        y += b[i] * x[i]
+    return y
+
+
+def get_rest_s_dict(x_dict: dict):
+    index = [30,  40,  46,  62,  82,  83,  90, 153, 159, 183, 185, 222, 251, 252, 265, 271, 282, 294, 325,  4, 6]
+    x = []
+    for e in index:
+        x.append(x_dict[e])
+    return get_rest_s(x)
+
+
 if __name__ == "__main__":
     df_x = pandas.read_excel("./data/x1.xlsx")
     df_y = pandas.read_excel("./data/y1.xlsx")
@@ -129,4 +168,4 @@ if __name__ == "__main__":
         base = get_new_ron(record_list[i], i)
         greedy(record_list[i], base, i)
 
-    # greedy(record_list[132], df_y.loc[132, 1], 132)
+    # greedy(record_list[132], get_new_ron(record_list[132], 132), 132)
